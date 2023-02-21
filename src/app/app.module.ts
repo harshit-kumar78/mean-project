fd3ab5aabb13e3e52cfff7c91b9f170b13633861
harshit-kumar78 <harshit3551@gmail.com>
2023-02-21 17:23:21 +0530

    register component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,34 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { AccountserviceService } from './account/accountservice.service';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BannerComponent } from './banner/banner.component';
-import { HeaderComponent } from './header/header.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { ArticlesModule } from './articles/articles.module';
 import { StaticpagesModule } from './staticpages/staticpages.module';
+import { AccountModule } from './account/account.module';
+
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     FooterComponent,
     BannerComponent,
-    HeaderComponent,
     PagenotfoundComponent,
   ],
-  imports: [BrowserModule, ArticlesModule, StaticpagesModule, AppRoutingModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ArticlesModule,
+    StaticpagesModule,
+    AccountModule,
+    AppRoutingModule,
+  ],
+  providers: [AccountserviceService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
